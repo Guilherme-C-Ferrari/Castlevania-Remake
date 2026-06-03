678fd7681fe344dd46b5bc8508514aced52e643f
Guilherme Carvalho Ferrari <guicferrari@hotmail.com>
2026-06-02 22:23:56 -0300

    Scripts Iniciais de FSM

---
+++ Scripts/FSM/MoveLinear.gd
@@ -0,0 +1,10 @@
+extends State
+class_name MoveLinear
+
+@export var character: CharacterBody2D
+@export var move_speed: float = 75.0
+@export var walk_direction: Vector2
+	
+func physics_update(delta: float) -> void:
+	if character.is_on_floor():
+		character.velocity = walk_direction * move_speed
