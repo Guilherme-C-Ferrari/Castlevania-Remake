79dde542a943ee25b80a0e85fca8dfe698eb9990
Guilherme Carvalho Ferrari <guicferrari@hotmail.com>
2026-06-05 20:34:28 -0300

    Refatoração dos scripts do Zombie

--- Scripts/Enemy/zombie.gd
+++ Scripts/Enemy/zombie.gd
@@ -1,16 +1,7 @@
 extends CharacterBody2D
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @export var walk_direction: Vector2
+@export var move_speed: float = 75
 
 func _ready() -> void:
+	$FSM/MoveLinear.move_speed = move_speed
 	$FSM/MoveLinear.walk_direction = walk_direction
-
-func _physics_process(delta: float) -> void:
-	var move_direction = velocity.normalized()
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-	if move_direction.x > 0.0:
-		animated_sprite.flip_h = true
-	elif move_direction.x < 0.0:
-		animated_sprite.flip_h = false
-	move_and_slide()
--- Scripts/FSM/MoveLinear.gd
+++ Scripts/FSM/MoveLinear.gd
@@ -2,9 +2,20 @@ extends State
 class_name MoveLinear
 
 @export var character: CharacterBody2D
-@export var move_speed: float = 75.0
+@export var animated_sprite: AnimatedSprite2D
+@export var move_speed: float
 @export var walk_direction: Vector2
-	
+
 func physics_update(_delta: float) -> void:
 	if character.is_on_floor():
 		character.velocity = walk_direction * move_speed
+	
+	var move_direction = character.velocity.normalized()
+	
+	if not character.is_on_floor():
+		character.velocity += character.get_gravity() * _delta
+	if move_direction.x > 0.0:
+		animated_sprite.flip_h = true
+	elif move_direction.x < 0.0:
+		animated_sprite.flip_h = false
+	character.move_and_slide()
