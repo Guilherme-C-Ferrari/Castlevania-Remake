ea651924562edf4c63666f296e823746440ca888
Guilherme Carvalho Ferrari <guicferrari@hotmail.com>
2026-06-05 19:38:55 -0300

    Zombie com Sprites finais

--- Scripts/Enemy/zombie.gd
+++ Scripts/Enemy/zombie.gd
@@ -1,6 +1,12 @@
 extends CharacterBody2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
+	var move_direction = velocity.normalized()
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	if move_direction.x > 0.0:
+		animated_sprite.flip_h = true
+	elif move_direction.x < 0.0:
+		animated_sprite.flip_h = false
 	move_and_slide()
