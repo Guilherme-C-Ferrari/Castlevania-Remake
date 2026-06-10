43fa70bf33984b2634ef8063bc910d8ad6fc28e4
Guilherme Carvalho Ferrari <guicferrari@hotmail.com>
2026-06-09 21:33:13 -0300

    Scripts estado base Black Panther

---
+++ Scripts/FSM/Idling.gd
@@ -0,0 +1,10 @@
+extends State
+class_name Idling
+
+@onready var character: CharacterBody2D
+@onready var animated_sprite: AnimatedSprite2D
+
+func _ready() -> void:
+	character = owner as CharacterBody2D
+	if character:
+		animated_sprite = character.get_node_or_null("AnimatedSprite2D")
