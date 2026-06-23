aa5d8ded371d61ff6bda2b7e57d7cd8bc4d8aa3b
EDUARDO SPODE CARDOSO <eduardo.spode01@edu.pucrs.br>
2026-06-22 21:02:31 -0300

    enemy facing

--- Scripts/Player/player_stats_controller.gd
+++ Scripts/Player/player_stats_controller.gd
@@ -2,9 +2,9 @@ extends Node
 
 @onready var player = get_tree().get_first_node_in_group("player")
 
-func receive_damage(damage: int):
+func receive_damage(damage: int, facing: int):
 	Ui.remove_player_health(damage)
-	var enemy_facing := Vector2(1,0)
+	var enemy_facing := Vector2(facing,0)
 	
 	if player:
 		player.take_hit(enemy_facing)
