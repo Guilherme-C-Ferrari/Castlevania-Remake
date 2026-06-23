dc1aa23ddacca8e619bbd8aa7012b7937a921e6c
Guilherme Carvalho Ferrari <guicferrari@hotmail.com>
2026-06-23 04:11:37 -0300

    Fix 2

--- Scripts/Enemy/LongSpawner.gd
+++ Scripts/Enemy/LongSpawner.gd
@@ -12,19 +12,11 @@ func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
 	timer.timeout.connect(verify_spawn)
-	
-	var player = get_tree().get_first_node_in_group("player")
-	if player:
-		var min_x = global_position.x - (zone_size.x / 2.0)
-		var max_x = global_position.x + (zone_size.x / 2.0)
-		if player.global_position.x >= min_x and player.global_position.x <= max_x:
-			player_inside = true
 
 func _physics_process(_delta: float) -> void:
 	spawned_enemies = spawned_enemies.filter(func(enemy): return is_instance_valid(enemy))
 
 func verify_spawn() -> void:
-	print(player_inside)
 	if not player_inside or spawned_enemies.size() >= max_qty_to_spawn:
 		return
 	
@@ -53,7 +45,6 @@ func spawn_mob_random_pos(spawn_x: float) -> void:
 		var direction = sign(player.global_position.x - spawn_x)
 		enemy.walk_direction = direction
 	get_parent().add_child(enemy)
-	print("spawn")
 	spawned_enemies.append(enemy)
 
 func _on_body_entered(body: Node2D) -> void:
@@ -62,5 +53,4 @@ func _on_body_entered(body: Node2D) -> void:
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		print("a")
 		player_inside = false
