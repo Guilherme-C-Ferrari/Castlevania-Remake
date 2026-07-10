bf9ced1dd60be949133a19b27e339170a92d0827
Guilherme Carvalho Ferrari <guicferrari@hotmail.com>
2026-07-10 01:27:20 -0300

    Shooting lógica base

--- Scripts/FSM/Following.gd
+++ Scripts/FSM/Following.gd
@@ -8,6 +8,12 @@ func enter() -> void:
 	if animated_sprite:
 		if not animated_sprite.is_playing() or animated_sprite.animation != "Moving":
 			animated_sprite.play("Moving")
+	if character and character.get("can_shoot") == true:
+		var timer = character.get_node_or_null("Timer")
+		if timer:
+			timer.timeout.connect(_on_timer_timeout)
+			if timer.is_stopped():
+				timer.start()
 
 func physics_update(_delta: float) -> void:
 	if not player:
@@ -38,9 +44,17 @@ func physics_update(_delta: float) -> void:
 	handle_animation()
 	character.move_and_slide()
 
+func exit() -> void:
+	if character and character.get_node_or_null("Timer"):
+		if character.get_node("Timer").timeout.is_connected(_on_timer_timeout):
+			character.get_node("Timer").timeout.disconnect(_on_timer_timeout)
+
 func handle_animation() -> void:
 	var move_direction = character.velocity.normalized()
 	if move_direction.x > 0.0:
 		animated_sprite.flip_h = true
 	elif move_direction.x < 0.0:
 		animated_sprite.flip_h = false
+
+func _on_timer_timeout() -> void:
+	transitioned.emit(self, "Shooting")
--- Scripts/FSM/Shooting.gd
+++ Scripts/FSM/Shooting.gd
@@ -6,5 +6,5 @@ func enter() -> void:
 		if not animated_sprite.is_playing() or animated_sprite.animation != "Shooting":
 			animated_sprite.play("Shooting")
 	print("Shooting")
-	await get_tree().create_timer(3.0).timeout
-	transitioned.emit("Following")
+	await get_tree().create_timer(2.0).timeout
+	transitioned.emit(self, "Following")
