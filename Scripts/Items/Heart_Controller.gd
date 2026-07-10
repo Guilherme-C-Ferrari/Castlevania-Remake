4f7244133079ffe73674776d64f63d689a545638
Edu-Dev12 <eduardospode@hotmail.com>
2026-07-09 23:37:04 -0300

    add heart, wip_upgrade and all resource

---
+++ Scripts/Items/Heart_Controller.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name Heart_Controller
+
+func heart_collected(hearts:int):
+	Ui.add_extra_point(hearts)
--- Scripts/Items/item_drop.gd
+++ Scripts/Items/item_drop.gd
@@ -5,12 +5,21 @@ extends CharacterBody2D
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var tempo: float = 0.0
+var velocidade_onda: float = 8.0 
+var amplitude_onda: float = 90.0 
+
 func _ready() -> void:
 	item_sprite.texture = item_resource.sprite
 
 func _physics_process(delta):
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		if item_resource and item_resource.name.to_lower() == "heart":
+			velocity.y += (gravity - 800) * delta
+			tempo += delta * velocidade_onda
+			velocity.x = cos(tempo) * amplitude_onda
+		else:
+			velocity.y += gravity * delta
 	else:
 		velocity.y = 0
 		velocity.x = 0 
