3773ae24f7be187764608e9ef99cee632bfdaa18
Edu-Dev12 <eduardospode@hotmail.com>
2026-07-11 03:18:45 -0300

    add jarro

--- Scripts/Items/Special_Controller.gd
+++ Scripts/Items/Special_Controller.gd
@@ -1,9 +1,22 @@
 extends Resource
 class_name Special_Controller
 
-const CHANGE_COLOR = preload("uid://be7po518l0752")
-const WEAPON_OBTAINED_SFX = preload("uid://clswbxpktn0qf")
+const CHANGE_COLOR_SHADER = preload("uid://be7po518l0752")
+const INVENSIBLE_SHADER = preload("uid://d00ygg04g3wai")
 
+
+const WEAPON_OBTAINED_SFX = preload("uid://clswbxpktn0qf")
+const CROSS_FLASHES_SFX = preload("uid://d2plcr0i5p5hl")
+
+var player = Engine.get_main_loop().get_first_node_in_group("player")
+
+func _init() -> void:
+	SignalManager.player_spawned.connect(update_player_reference)
+
+func update_player_reference(new_player: CharacterBody2D):
+	player = new_player
+
+var flashes_count := 14
 func special_collected(name: String):
 	if name == "wip_upgrade":
 		upgrade_wip()
@@ -20,21 +33,22 @@ func upgrade_wip():
 	var player = tree.get_first_node_in_group("player")
 	
 	if player:
-		var wip_sprite = player.get_node("Visual/AnimatedSpritePlayer")
+		var player_sprite = player.get_node("Visual/AnimatedSpritePlayer")
 		
 		tree.paused = true
 		
-		if wip_sprite:
-			wip_sprite.material = CHANGE_COLOR
+		if player_sprite:
+			player_sprite.material = CHANGE_COLOR_SHADER
 			
 		await tree.create_timer(1.0, true).timeout
 		
-		if wip_sprite:
-			wip_sprite.material = null
+		if player_sprite:
+			player_sprite.material = null
 			
 		tree.paused = false
 	
 func cruz_effect():
+	AudioManager.play_sound_effect(CROSS_FLASHES_SFX, "SFX", -6)
 	var enemies = Engine.get_main_loop().get_nodes_in_group("enemy")
 	for enemy in enemies:
 		if enemy.is_on_screen:
@@ -42,7 +56,7 @@ func cruz_effect():
 			
 	var color_black = true
 	var tree = Engine.get_main_loop()
-	for i in range(14):
+	for i in range(flashes_count):
 		if color_black:
 			RenderingServer.set_default_clear_color(Color.html("#FFFFFFFF"))
 			color_black = false
@@ -51,5 +65,44 @@ func cruz_effect():
 			color_black = true
 		
 		await tree.create_timer(0.02, true).timeout
+		
 func jarro_effect():
-	pass
+	start_invincibility()
+	
+func start_invincibility() -> void:
+	var tree = Engine.get_main_loop()
+	
+	player.invincible = true
+	player.set_collision_layer_value(2, false)
+	
+	var tween = player.get_tree().create_tween()
+	tween.tween_property(player.visual, "modulate:a", 0.3, 0.15)
+	
+	await tree.create_timer(2.0, true).timeout
+	
+	var player_sprite = player.get_node("Visual/AnimatedSpritePlayer")
+	
+	if player_sprite:
+			player_sprite.material = INVENSIBLE_SHADER
+	
+	await tree.create_timer(3.0, true).timeout
+	
+	end_invincibility()
+
+
+func end_invincibility() -> void:
+	print("ACABOU")
+	var tree = Engine.get_main_loop()
+	
+	var player_sprite = player.get_node("Visual/AnimatedSpritePlayer")
+	
+	if player_sprite:
+			player_sprite.material = null
+	
+	await tree.create_timer(2.0, true).timeout
+	
+	player.set_collision_layer_value(2, true)
+	player.invincible = false
+	
+	var tween = player.get_tree().create_tween()
+	tween.tween_property(player.visual, "modulate:a", 1.0, 0.15)
