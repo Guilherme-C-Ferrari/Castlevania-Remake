4c34dba3c6d76bbfbdbf5427563196c9b07fff29
Guilherme Carvalho Ferrari <guicferrari@hotmail.com>
2026-07-14 18:48:45 -0300

    Boss death explosion

--- Scripts/Enemy/BaseBoss.gd
+++ Scripts/Enemy/BaseBoss.gd
@@ -2,6 +2,7 @@ extends BaseEnemy
 class_name BaseBoss
 
 @onready var is_active : bool = false
+const boss_fire_explosion = preload("res://Scenes/Effect/boss_fire_explosion.tscn")
 
 func _ready() -> void:
 	super()
@@ -12,6 +13,9 @@ func _physics_process(_delta: float) -> void:
 func activate() -> void:
 	is_active = true
 
+func drop_item() -> void:
+	pass
+
 func on_receive_damage(amount: int) -> void:
 	AudioManager.play_sound_effect(enemy_destroyer, "SFX", -12, 0.85)
 	spawn_explosion()
@@ -20,5 +24,16 @@ func on_receive_damage(amount: int) -> void:
 	if hp <= 0:
 		die()
 
-func drop_item() -> void:
-	pass
+func spawn_boss_explosion() -> void:
+	var boss_explosion = boss_fire_explosion.instantiate()
+	boss_explosion.global_position = sprite.global_position
+	get_parent().add_child(boss_explosion)
+
+func die() -> void:
+	AudioManager.play_sound_effect(enemy_destroyer, "SFX", -12, 0.85)
+	sprite.set_deferred("visible", false)
+	hit_box.get_child(0).set_deferred("disabled", true)
+	Ui.add_score(experience)
+	spawn_boss_explosion()
+	drop_item()
+	queue_free()
