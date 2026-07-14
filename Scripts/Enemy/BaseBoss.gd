d9480973d62681b1d503743f15cfc9ae228e1e6e
Guilherme Carvalho Ferrari <guicferrari@hotmail.com>
2026-07-14 19:00:50 -0300

    Boss drop

--- Scripts/Enemy/BaseBoss.gd
+++ Scripts/Enemy/BaseBoss.gd
@@ -1,8 +1,10 @@
 extends BaseEnemy
 class_name BaseBoss
 
+@export var drop_position : Marker2D
 @onready var is_active : bool = false
 const boss_fire_explosion = preload("res://Scenes/Effect/boss_fire_explosion.tscn")
+const ORB_RES = preload("uid://8xs0auhitwid")
 
 func _ready() -> void:
 	super()
@@ -14,7 +16,15 @@ func activate() -> void:
 	is_active = true
 
 func drop_item() -> void:
-	pass
+	spawn_drop.call_deferred(ORB_RES)
+
+func spawn_drop(item_resource: Resource) -> void:
+	if not item_resource or not ITEM_DROP_SCENE: return
+	var drop_instance = ITEM_DROP_SCENE.instantiate()
+	if "item_resource" in drop_instance:
+		drop_instance.item_resource = item_resource
+	get_parent().add_child(drop_instance)
+	drop_instance.global_position = drop_position.global_position
 
 func on_receive_damage(amount: int) -> void:
 	AudioManager.play_sound_effect(enemy_destroyer, "SFX", -12, 0.85)
